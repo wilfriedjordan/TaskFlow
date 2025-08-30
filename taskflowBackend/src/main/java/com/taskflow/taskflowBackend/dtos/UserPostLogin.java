49de895634bae67af22f04dabe6bd03533d02e4e
wilfriedjordan <tankwewilfried5@gmail.com>
2025-08-30 06:15:53 +0100

    30-08-2025

--- taskflowBackend/src/main/java/com/taskflow/taskflowBackend/dtos/UserPostLogin.java
+++ taskflowBackend/src/main/java/com/taskflow/taskflowBackend/dtos/UserPostLogin.java
@@ -6,8 +6,9 @@ import java.util.List;
 
 @Data
 public class UserPostLogin {
+    private String token;
+
     private String username;
     private String email;
-    private String token;
     private List<String> roles;
 }
